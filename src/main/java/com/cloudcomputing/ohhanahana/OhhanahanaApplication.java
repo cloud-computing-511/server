16d88c1c6c424cb082b504fa35ff47b9445ae76a
준써크 <junsuk8960@naver.com>
2024-06-01 11:27:55 +0900

    initial commit

---
+++ src/main/java/com/cloudcomputing/ohhanahana/OhhanahanaApplication.java
@@ -0,0 +1,13 @@
+package com.cloudcomputing.ohhanahana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhhanahanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhhanahanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudcomputing/ohhanahana/OhhanahanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudcomputing.ohhanahana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhhanahanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
