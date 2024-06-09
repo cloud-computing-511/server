4ddb708be9d0c3659a20132e7784686c30dd5476
Junseok Kim <30834810+junseokkim@users.noreply.github.com>
2024-06-09 21:35:09 +0900

    Update deploy.sh
    기존 컨테이너 삭제 전 존재 여부 판단 로직 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,10 +4,15 @@
 cd /home/ubuntu/deployment
 
 # 기존 컨테이너 중지 및 삭제
-sudo docker stop $(sudo docker ps -a -q --filter "name=ohhanahana-app") || true
-sudo docker rm $(sudo docker ps -a -q --filter "name=ohhanahana-app") || true
+CONTAINER_NAME="ohhanahana-app"
+EXISTING_CONTAINER=$(sudo docker ps -a -q --filter "name=$CONTAINER_NAME")
 
-# 최신 이미지 가져오기de
+if [ -n "$EXISTING_CONTAINER" ]; then
+  sudo docker stop $EXISTING_CONTAINER
+  sudo docker rm $EXISTING_CONTAINER
+fi
+
+# 최신 이미지 가져오기
 sudo docker pull $ECR_REGISTRY/$ECR_REPOSITORY:latest
 
 # 새 컨테이너 실행
