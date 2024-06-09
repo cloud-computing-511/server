1f2b9b83adb6fc1d826fdc8fe4caa91b5876e96b
준써크 <junsuk8960@naver.com>
2024-06-10 01:51:43 +0900

    fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,57 +4,18 @@
 source /home/ubuntu/deployment/scripts/env.sh
 
 # ECR 로그인
-aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_REGISTRY
-
-echo "ECR_REGISTRY=$ECR_REGISTRY"
-echo "ECR_REPOSITORY=$ECR_REPOSITORY"
+aws ecr get-login-password --region $AWS_REGION | sudo docker login --username AWS --password-stdin $ECR_REGISTRY
 
 cd /home/ubuntu/deployment
 
 CONTAINER_NAME="ohhanahana-app"
-EXISTING_CONTAINER=$(docker ps -a -q --filter "name=$CONTAINER_NAME")
+EXISTING_CONTAINER=$(sudo docker ps -a -q --filter "name=$CONTAINER_NAME")
 
 if [ -n "$EXISTING_CONTAINER" ]; then
-  docker stop $EXISTING_CONTAINER
-  docker rm $EXISTING_CONTAINER
+  sudo docker stop $EXISTING_CONTAINER
+  sudo docker rm $EXISTING_CONTAINER
 fi
 
 IMAGE_NAME="${ECR_REGISTRY}/${ECR_REPOSITORY}:latest"
-echo "IMAGE_NAME=$IMAGE_NAME"
-docker pull $IMAGE_NAME
-docker run -d --name $CONTAINER_NAME -p 8080:8080 $IMAGE_NAME
-
-
-
-
-##!/bin/bash
-#
-## 환경 변수 로드
-#source /home/ubuntu/deployment/scripts/env.sh
-#
-## 환경 변수 확인 (디버깅을 위해 추가)
-#echo "ECR_REGISTRY: $ECR_REGISTRY"
-#echo "ECR_REPOSITORY: $ECR_REPOSITORY"
-#
-## AWS ECR 로그인
-#aws ecr get-login-password --region $AWS_REGION | sudo docker login --username AWS --password-stdin $ECR_REGISTRY
-#
-## 디렉토리로 이동
-#cd /home/ubuntu/deployment
-#
-## 기존 컨테이너 중지 및 삭제
-#CONTAINER_NAME="ohhanahana-app"
-#EXISTING_CONTAINER=$(sudo docker ps -a -q --filter "name=$CONTAINER_NAME")
-#
-#if [ -n "$EXISTING_CONTAINER" ]; then
-#  sudo docker stop $EXISTING_CONTAINER
-#  sudo docker rm $EXISTING_CONTAINER
-#fi
-#
-## 최신 이미지 가져오기
-#IMAGE_NAME="$ECR_REGISTRY/$ECR_REPOSITORY:latest"
-#echo "IMAGE_NAME: $IMAGE_NAME"
-#sudo docker pull $IMAGE_NAME
-#
-## 새 컨테이너 실행
-#sudo docker run -d --name $CONTAINER_NAME -p 8080:8080 $IMAGE_NAME
+sudo docker pull $IMAGE_NAME
+sudo docker run -d --name $CONTAINER_NAME -e SPRING_PROFILE=main -e TZ=Asia/Seoul -p 8080:8080 $IMAGE_NAME
