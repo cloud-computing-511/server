41b4529e5c98f52572f44184f8a81926689ace9a
gourderased <117848386+gourderased@users.noreply.github.com>
2024-06-19 19:25:39 +0900

    Feat/#9 혼잡도 조회
    * FEAT : 다이나모 기본 설정
    
    * FEAT : gitignore 추가
    
    * FEAT : 센서 데이터 api
    
    * FEAT : build.gradle 수정
    
    * FEAT : 혼잡도 조회 구현
    
    * FIX : 혼잡도 조회 수정
    
    * FIX : 예상 대기 시간 수정

--- src/main/java/com/cloudcomputing/ohhanahana/repository/SensorDataRepository.java
+++ src/main/java/com/cloudcomputing/ohhanahana/repository/SensorDataRepository.java
@@ -1,5 +1,11 @@
 package com.cloudcomputing.ohhanahana.repository;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+import com.cloudcomputing.ohhanahana.dto.response.SensorDataResponse;
+import com.cloudcomputing.ohhanahana.entity.SensorData;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -7,7 +13,25 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 public class SensorDataRepository {
 
-    public void findLatest() {
-        //TODO - 다이나모 디비에서 최신값 불러오기
+    private final DynamoDBMapper dynamoDBMapper;
+
+    public List<SensorData> findAll() {
+        return dynamoDBMapper.scan(SensorData.class, new DynamoDBScanExpression());
+    }
+
+    public SensorData findLatestByDeviceId(String deviceId) {
+        DynamoDBQueryExpression<SensorData> queryExpression = new DynamoDBQueryExpression<SensorData>()
+            .withHashKeyValues(SensorData.builder().deviceId(deviceId).build())
+            .withScanIndexForward(true) //오름차순
+            .withLimit(50); //가장 최신값 하나만 가져오기
+
+        List<SensorData> result = dynamoDBMapper.query(SensorData.class, queryExpression);
+
+        if(result.isEmpty()) {
+            return null;
+        }
+        else {
+            return result.get(0);
+        }
     }
 }
--- src/main/java/com/cloudcomputing/ohhanahana/service/SensorDataService.java
+++ src/main/java/com/cloudcomputing/ohhanahana/service/SensorDataService.java
@@ -1,8 +1,15 @@
 package com.cloudcomputing.ohhanahana.service;
 
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.cloudcomputing.ohhanahana.dto.response.SensorDataResponse;
+import com.cloudcomputing.ohhanahana.entity.SensorData;
 import com.cloudcomputing.ohhanahana.enums.Congestion;
 import com.cloudcomputing.ohhanahana.repository.SensorDataRepository;
+import jakarta.xml.bind.JAXBException;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,16 +18,88 @@ import org.springframework.stereotype.Service;
 public class SensorDataService {
 
     private final SensorDataRepository sensorDataRepository;
+    private final BusService busService;
 
-    public SensorDataResponse findSensorData() {
-        sensorDataRepository.findLatest();
+    public SensorDataResponse findLatestSensorData() throws JAXBException {
+        Congestion congestion = null;
 
-        String congestion = Congestion.NORMAL.getToKorean();
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm a");
 
-        return SensorDataResponse.toDTO("인하대학교",
-            "2024-05-31 16:30 PM",
+        //시간 체크(10 : 00 ~ 20 : 00)
+        LocalTime currentTime = LocalTime.now();
+        LocalTime startTime = LocalTime.of(10,0);
+        LocalTime endTime = LocalTime.of(22,0);
+
+        if(currentTime.isBefore(startTime) || currentTime.isAfter(endTime)) {
+            congestion = Congestion.SENSOR_INACTIVE;
+        }
+
+        else {
+            SensorData sensorData1 = sensorDataRepository.findLatestByDeviceId("dev_1");
+            SensorData sensorData2 = sensorDataRepository.findLatestByDeviceId("dev_2");
+
+            if(sensorData1 != null && sensorData2 != null) {
+                Map<String, AttributeValue> payload1 = sensorData1.getPayload();
+                Map<String, AttributeValue> payload2 = sensorData2.getPayload();
+
+                int sensor1Value = Integer.parseInt(payload1.get("Sensor1").getN());
+                int sensor2Value = Integer.parseInt(payload1.get("Sensor2").getN());
+                int sensor3Value = Integer.parseInt(payload2.get("Sensor3").getN());
+                int sensor4Value = Integer.parseInt(payload2.get("Sensor4").getN());
+
+                congestion = determineCongestion(sensor1Value, sensor2Value, sensor3Value, sensor4Value);
+
+            }
+            else {
+                congestion = Congestion.SENSOR_ERROR;
+            }
+        }
+
+        return SensorDataResponse.toDTO(
+            "인하대학교",
+            now.format(formatter),
             congestion,
-            25,
-            20);
+            calculateWaitingTime(congestion),
+            calculatePeople(congestion)
+        );
+    }
+
+    private Congestion determineCongestion(int sensor1Value, int sensor2Value, int sensor3Value, int sensor4Value) {
+        if (isWithinRange(sensor1Value) && isWithinRange(sensor2Value)) {
+            if (isWithinRange(sensor3Value) && isWithinRange(sensor4Value)) {
+                return Congestion.CONGESTION;
+            } else {
+                return Congestion.NORMAL;
+            }
+        } else {
+            return Congestion.SPARE;
+        }
+    }
+
+    private String calculateWaitingTime(Congestion congestion) throws JAXBException {
+        int remain511Minute = busService.get511BusRemainMinute();
+
+        return switch (congestion) {
+            case SPARE -> remain511Minute + "분";
+            case NORMAL -> remain511Minute + 5 + "분";
+            case CONGESTION -> remain511Minute + 10 + "분";
+            default -> "-";
+        };
+    }
+
+    private String calculatePeople(Congestion congestion) {
+        return switch (congestion) {
+            case SPARE -> "15명";
+            case NORMAL -> "25명";
+            case CONGESTION -> "35명";
+            default -> "-";
+        };
+    }
+
+    private boolean isWithinRange(int value) {
+        int SENSOR_MAX_VALUE = 150;
+        int SENSOR_MIN_VALUE = 1;
+        return value >= SENSOR_MIN_VALUE && value <= SENSOR_MAX_VALUE;
     }
 }
